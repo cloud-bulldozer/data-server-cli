ec8a2cc52f94721a33bb7bedf8c0766bf5f7c638
mleader <mleader@redhat.com>
2020-07-24 13:38:05 -0400

    trying .toml

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import setuptools
+import setuptools, os, io
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+CURDIR = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
+    README = f.read()
+
+
 
 setuptools.setup(
-    name='snappy',
+    name='snappycli',
     version = '0.0.1',
     author='Matthew F Leader',
     author_email='mleader@redhat.com',
     description='client and command line interface to a snappy data server',
-    long_description = long_description,
-    download_url='https://github.com/mfleader/snappy-client',
+    long_description = README,
+    long_description_content_type = 'text/markdown',
+    download_url='https://github.com/mfleader/snappyCLI',
     package=setuptools.find_packages(),
     entry_points={
         'console_scripts': [

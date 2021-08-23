523ffef8de066fbb822dff532e4ad2d948653cee
Harshith-umesh <harshith.umesh.nat@gmail.com>
2021-08-23 18:00:27 +0530

    commit2

--- snappycli/client.py
+++ snappycli/client.py
@@ -1,11 +1,9 @@
 import sys
-
 from pathlib import Path
 from typing import AsyncGenerator
 
 import aiofiles
 import httpx
-
 from aiofiles import os as aios
 from toolz import pipe
 from tqdm import tqdm
@@ -15,45 +13,30 @@ def response_handler(r: httpx.Response) -> dict:
     try:
         r.raise_for_status()
     except httpx.HTTPStatusError as e:
-        detail = r.json().get('detail', None)
-        cause = '\n'.join((detail,str(e))) if detail else str(e)
+        detail = r.json().get("detail", None)
+        cause = "\n".join((detail, str(e))) if detail else str(e)
         raise Exception(cause).with_traceback(e.__traceback__)
     return r.json()
 
 
-def _login_req(
-    url: str, username: str, password: str
-) -> httpx.Response:
-    return httpx.post(
-        url,
-        data={
-            'username': username,
-            'password': password
-        })
+def _login_req(url: str, username: str, password: str) -> httpx.Response:
+    return httpx.post(url, data={"username": username, "password": password})
 
 
 def token(url: str, username: str, password: str) -> str:
-    return pipe(
-        _login_req(url, username, password),
-        response_handler,
-        lambda r: r['access_token']
-    )
+    return pipe(_login_req(url, username, password), response_handler, lambda r: r["access_token"])
 
 
-async def upload_bytes(            
-    filepath: Path,
-    chunk_size: int = 524_288_000
-) -> AsyncGenerator[bytes, None]:
+async def upload_bytes(filepath: Path, chunk_size: int = 524_288_000) -> AsyncGenerator[bytes, None]:
     # 500 MiB == 500 * 1024 * 1024 == 524,288,000
-    contents = 'dummy'
+    contents = "dummy"
     pointer = 0
-    async with aiofiles.open(filepath, 'rb') as file:
-        while len(contents):
+    async with aiofiles.open(filepath, "rb") as file:
+        while contents:
             await file.seek(pointer)
             pointer += chunk_size
             contents = await file.read(chunk_size)
             yield contents
-
 
 
 async def show_progress_bar(filepath: Path) -> AsyncGenerator[bytes, None]:
@@ -64,40 +47,30 @@ async def show_progress_bar(filepath: Path) -> AsyncGenerator[bytes, None]:
         unit_scale=True,
         unit_divisor=1024,
         file=sys.stdout,
-        desc=f'Posting {filepath.name}'
+        desc=f"Posting {filepath.name}",
     ) as progress:
         async for contents in upload_bytes(filepath):
             progress.update(float(len(contents)))
-            yield contents            
+            yield contents
 
 
 async def _async_post_file(
-    url: str, tkn: str, filepath: Path, filedir: str = '', 
-    silent: bool = False
+    url: str, tkn: str, filepath: Path, filedir: str = "", silent: bool = False
 ) -> httpx.Response:
-    
-    async with httpx.AsyncClient(timeout=httpx.Timeout(
-                write=None, read=None, connect=None, pool=None)) as client:
+
+    async with httpx.AsyncClient(
+        timeout=httpx.Timeout(write=None, read=None, connect=None, pool=None)
+    ) as client:
         r = await client.post(
             url,
-            params={
-                'filename': filepath.name,
-                'filedir' : filedir
-            },
-            data=upload_bytes(filepath) if silent \
-              else show_progress_bar(filepath),
-            headers={
-                'Authorization': f'Bearer {tkn}'
-            }
+            params={"filename": filepath.name, "filedir": filedir},
+            data=upload_bytes(filepath) if silent else show_progress_bar(filepath),
+            headers={"Authorization": f"Bearer {tkn}"},
         )
     return r
 
 
-async def async_post_file(
-    url: str, tkn: str, filepath: Path, filedir: str = '', silent: bool = False
-) -> str:
+async def async_post_file(url: str, tkn: str, filepath: Path, filedir: str = "", silent: bool = False) -> str:
     return pipe(
-        await _async_post_file(url, tkn, filepath, filedir, silent),
-        response_handler,
-        lambda r: r['loc']
+        await _async_post_file(url, tkn, filepath, filedir, silent), response_handler, lambda r: r["loc"]
     )
--- snappycli/main.py
+++ snappycli/main.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
+from functools import wraps
 from pathlib import Path
 from typing import Callable
-from functools import wraps
 
 import typer
 from toolz import pipe
@@ -17,6 +17,7 @@ app = typer.Typer()
 
 def exception_handler(fn: Callable) -> Callable:
     if asyncio.iscoroutinefunction(fn):
+
         @wraps(fn)
         async def wrapper(*args, **kwargs):
             try:
@@ -24,121 +25,95 @@ def exception_handler(fn: Callable) -> Callable:
             except Exception as e:
                 typer.echo(e)
                 raise typer.Abort()
+
         return wrapper
     else:
+
         @wraps(fn)
         def wrapper(*args, **kwargs):
             try:
                 return fn(*args, **kwargs)
             except Exception as e:
-                typer.echo(e)                
+                typer.echo(e)
                 raise typer.Abort()
+
         return wrapper
 
 
 @exception_handler
-async def _async_post_file(
-    url: str, token: str, filepath: Path, filedir: str, silent: bool
-) -> str:
-    r = await client.async_post_file(
-        url=url, tkn=token, filepath=filepath, filedir=filedir,
-        silent=silent
-    )
+async def _async_post_file(url: str, token: str, filepath: Path, filedir: str, silent: bool) -> str:
+    r = await client.async_post_file(url=url, tkn=token, filepath=filepath, filedir=filedir, silent=silent)
     return r
 
 
 @exception_handler
 def _login(url: str, username: str, password: str) -> str:
-    pipe(
-        auth.add(client.token(f'{url}/auth/jwt/login', username, password)),
-        auth.save
-    )
+    pipe(auth.add(client.token(f"{url}/auth/jwt/login", username, password)), auth.save)
 
 
 @app.command()
 def script_login(
-    data_server_username: str = typer.Argument(
-        ...,
-        envvar='DATA_SERVER_USERNAME'
-    ),
-    data_server_password: str = typer.Argument(
-        ...,
-        envvar='DATA_SERVER_PASSWORD'
-    ),
-    data_server_url: str = typer.Option(
-        'http://localhost:7070',
-        envvar='DATA_SERVER_URL'
-    )
+    data_server_username: str = typer.Argument(..., envvar="DATA_SERVER_USERNAME"),
+    data_server_password: str = typer.Argument(..., envvar="DATA_SERVER_PASSWORD"),
+    data_server_url: str = typer.Option("http://localhost:7070", envvar="DATA_SERVER_URL"),
 ) -> None:
     """
     Login to a snappy data server with a shell script
     using environment variables.
     """
-    _login(
-        data_server_url,
-        data_server_username,
-        data_server_password)
+    _login(data_server_url, data_server_username, data_server_password)
 
 
 @app.command()
 def login(
-    username: str = typer.Option(
-        ...,
-        prompt=True,
-        envvar='DATA_SERVER_USERNAME'
-    ),
+    username: str = typer.Option(..., prompt=True, envvar="DATA_SERVER_USERNAME"),
     password: str = typer.Option(
         ...,
-        prompt=True, hide_input=True, hidden=True,
+        prompt=True,
+        hide_input=True,
+        hidden=True,
     ),
-    url: str = typer.Option(
-        'http://localhost:7070',
-        envvar='DATA_SERVER_URL'
-    )
+    url: str = typer.Option("http://localhost:7070", envvar="DATA_SERVER_URL"),
 ) -> None:
     """
     Login to a snappy data server with a prompt.
     """
     _login(url, username, password)
-    typer.echo('login succeeded')
+    typer.echo("login succeeded")
 
 
 @app.command()
 def post_file(
     filepath: Path = typer.Argument(...),
-    url: str = typer.Option(
-        'http://localhost:7070',
-        envvar='DATA_SERVER_URL'
-    ),
-    filedir: str = typer.Option(
-        '',
-        envvar='SNAPPY_FILE_DIR'
-    ),
-    silent: bool = typer.Option(False, "-s", "--silent")
+    url: str = typer.Option("http://localhost:7070", envvar="DATA_SERVER_URL"),
+    filedir: str = typer.Option("", envvar="SNAPPY_FILE_DIR"),
+    silent: bool = typer.Option(False, "-s", "--silent"),
 ) -> None:
-    typer.echo(f"""your file is at {
+    typer.echo(
+        f"""your file is at {
     asyncio.run(_async_post_file(
         url=f'{url}/api',
         token=auth.token(auth.load()),
         filepath=filepath,
         filedir=filedir,
         silent=silent))
-    }""")
+    }"""
+    )
 
 
 @app.command()
 def logout() -> None:
     auth.save(auth.rm())
-    typer.echo('logged out of snappy')
+    typer.echo("logged out of snappy")
 
 
 @app.command()
 def install() -> None:
     """Automatically add required system resource for snappy cli"""
-    Path(Path.home(), '.snappy').mkdir(exist_ok=True)
+    Path(Path.home(), ".snappy").mkdir(exist_ok=True)
     auth.save(auth.rm())
-    typer.echo('snappy ready to go!')
+    typer.echo("snappy ready to go!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app()
